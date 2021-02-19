c850b7fc4eb68301fd5fd9c650185a3264512b97
Bharuthy <51150382+Bharuthy@users.noreply.github.com>
2021-02-19 11:55:12 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSliderModule } from '@angular/material/slider';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AccordianComponent } from './accordian/accordian.component';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { AccordianComponent } from './accordian/accordian.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
-    MatIconModule 
+    MatIconModule,
+    MatExpansionModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
