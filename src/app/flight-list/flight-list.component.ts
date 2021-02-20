1d65a004948cb3461ba59cd11bb004f203365d1d
Bharuthy <51150382+Bharuthy@users.noreply.github.com>
2021-02-20 20:33:00 +0530

    commit

--- src/app/flight-list/flight-list.component.ts
+++ src/app/flight-list/flight-list.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flight-list.component.scss']
 })
 export class FlightListComponent implements OnInit {
-
+  offerTab:any=1;
+  regTab:any=0;
+  infoTab:any=0;
   constructor() { }
-
+  
   ngOnInit(): void {
+  }
+  changetable(val:any){
+    debugger;
+    if(val==1){
+     this.offerTab = 1;
+     this.regTab = 0;
+     this.infoTab = 0;
+    }
+    if(val==2){
+      this.offerTab = 0;
+      this.regTab = 1;
+      this.infoTab = 0;
+     }
+     if(val==3){
+      this.offerTab = 0;
+      this.regTab = 0;
+      this.infoTab = 1;
+     }
   }
 
 }
